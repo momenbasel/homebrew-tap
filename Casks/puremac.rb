4aa575b2fb978a9ef16d5d4ff43a58586afd2941
moamen <mo@menbasel.com>
2026-04-15 01:14:50 +0200

    Update PureMac v2.0.0 hash
    Rebuilt with app uninstaller, orphan finder, and file removal wired up.

--- Casks/puremac.rb
+++ Casks/puremac.rb
@@ -1,6 +1,6 @@
 cask "puremac" do
   version "2.0.0"
-  sha256 "3aa36a7f4d52d810b5b947820d6cde2e688694264e724772c362e4ad9c6d8ace"
+  sha256 "590cdc26d0d11b83ebced7c7e91b5a2673fedd5cffc42a31e1305f9c37bc06ef"
 
   url "https://github.com/momenbasel/PureMac/releases/download/v#{version}/PureMac-#{version}.zip"
   name "PureMac"
