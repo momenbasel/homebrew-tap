7c72810bc92c4f16012bb6ff509367dded558852
moamen <mo@menbasel.com>
2026-04-15 01:34:09 +0200

    Update PureMac v2.0.0 hash (onboarding + safety)

--- Casks/puremac.rb
+++ Casks/puremac.rb
@@ -1,6 +1,6 @@
 cask "puremac" do
   version "2.0.0"
-  sha256 "1b73d6835fc1416a6601463d790c194a8e17219cc284d619dd3294764093e2ea"
+  sha256 "b0e89956773a90cd99f6f904e11376720fd11082e95ad7e4ae45cc8b3b1ac2c1"
 
   url "https://github.com/momenbasel/PureMac/releases/download/v#{version}/PureMac-#{version}.zip"
   name "PureMac"
