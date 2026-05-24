76875a634933c9f27c3287f7a94f932a0c168a13
moamen <mo@menbasel.com>
2026-05-24 09:13:55 +0300

    puremac: bump to 2.6.0 (sha256 ba143d27b593)

--- Casks/puremac.rb
+++ Casks/puremac.rb
@@ -1,6 +1,6 @@
 cask "puremac" do
-  version "2.5.2"
-  sha256 "4b22cc64b44350c4c90ecb58fb7f5704d765bbcb1f8976bd33f326e3950d6627"
+  version "2.6.0"
+  sha256 "ba143d27b593462b489c46b67ea7e36d9001b27b07588f18bdc85e4471908eed"
 
   url "https://github.com/momenbasel/PureMac/releases/download/v#{version}/PureMac-#{version}.zip"
   name "PureMac"
