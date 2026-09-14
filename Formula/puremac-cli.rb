66dad7d1ac461ee439ca64cd6296e21cc29af160
momenbasel <momenbasel@users.noreply.github.com>
2026-09-14 17:00:41 +0300

    puremac-cli: bump to 1.1.1 (sha256 c4ae4ae7fb04)

--- Formula/puremac-cli.rb
+++ Formula/puremac-cli.rb
@@ -1,9 +1,9 @@
 class PuremacCli < Formula
   desc "Clean developer caches, project artifacts, and disk clutter from the terminal"
   homepage "https://github.com/momenbasel/PureMac"
-  version "1.0.0"
-  url "https://github.com/momenbasel/PureMac/releases/download/cli-v#{version}/puremac-cli-#{version}.tar.gz"
-  sha256 "58cd364dc9204e37742312eaec19184041cf92bfdb91c53f64ed01b9e6c48519"
+  url "https://github.com/momenbasel/PureMac/releases/download/cli-v1.1.1/puremac-cli-1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "c4ae4ae7fb04a7d02b73d9f95a578ebf4c2f3636985d4966581818d3d8d5bcb2"
   license "MIT"
 
   depends_on macos: :big_sur
