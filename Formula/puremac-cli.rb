13c95da337023eb11399ae13bf6a823640774be7
momenbasel <momenbasel@users.noreply.github.com>
2026-09-14 17:02:10 +0300

    puremac-cli: use URL-derived version

--- Formula/puremac-cli.rb
+++ Formula/puremac-cli.rb
@@ -2,7 +2,6 @@ class PuremacCli < Formula
   desc "Clean developer caches, project artifacts, and disk clutter from the terminal"
   homepage "https://github.com/momenbasel/PureMac"
   url "https://github.com/momenbasel/PureMac/releases/download/cli-v1.1.1/puremac-cli-1.1.1.tar.gz"
-  version "1.1.1"
   sha256 "c4ae4ae7fb04a7d02b73d9f95a578ebf4c2f3636985d4966581818d3d8d5bcb2"
   license "MIT"
 
